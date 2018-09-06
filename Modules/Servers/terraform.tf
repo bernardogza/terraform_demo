679967feeca463ee04261c947130a2a6451d9820
Bernardo Garza <bgarza@bluecedar.com>
2018-09-06 16:23:38 -0500

    changes

--- Modules/Servers/terraform.tf
+++ Modules/Servers/terraform.tf
@@ -1,21 +1,4 @@
-/*variable "subscription_id" {
-  description = "The Azure subscription ID"
-  default = "113c4764-142e-4738-ae97-85bf6e509eb6"
-}
 
-variable "tenant_id" {
-  description = "The Azure tenant ID"
-   default = "c8cd0425-e7b7-4f3d-9215-7e5fa3f439e8"
-}
-
-variable "client_id" {
-  description = "The Azure client ID"
-   default = "e607356b-3ef4-4e4e-b134-ba5678eba330"
-}
-variable "secret_access_key" {
-  description = "The Azure secret access key" 
-   default = "tIGGnNGxUevgmq895/IPEFnbF5f+H9YS1Y4mZ0NpPfk="
-}*/
 variable "public_key" {
   description = "ssh public key"
 }
